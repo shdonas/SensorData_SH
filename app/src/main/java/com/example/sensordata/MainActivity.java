22960d22f37a8d9aea041ec89dbf7a9aacf40629
shdonas <BlackHole7473>
2019-11-23 12:45:53 -0500

    SensorData class added getters and setters added Pressure and humidity added

--- app/src/main/java/com/example/sensordata/MainActivity.java
+++ app/src/main/java/com/example/sensordata/MainActivity.java
@@ -1,10 +1,16 @@
+/**
+ * SensorData class
+ *
+ * @author          Shakhawat Hossain
+ * @version         1.0
+ * @since           11/23/2019
+ */
+
+
+
 package com.example.sensordata;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -13,8 +19,12 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+
 public class MainActivity extends AppCompatActivity
         implements SensorEventListener {
+    private static final String TAG = "Sensor Data";
 
     // System sensor manager instance.
     private SensorManager mSensorManager;
@@ -23,14 +33,35 @@ public class MainActivity extends AppCompatActivity
     private Sensor mSensorLight;
     private Sensor mSensorProximity;
     private Sensor mSensorTemp;
-
+    private Sensor mSensorHumid;
+    private Sensor mSensorPressure;
 
     // TextViews to display current sensor values.
     private TextView mTextSensorLight;
     private TextView mTextSensorProximity;
     private TextView mTextSensorTemp;
+    private TextView mTextSensorHumid;
+    private TextView mTextSensorPressure;
 
+    // sensor properties
+    private float ambient_light;
+    private float humidity;
+    private float pressure;
+    private float temperature;
 
+    /**
+     * Getting sensor data and display it
+     *
+     * Sensor used:
+     *  Ambient Light
+     *  Humidity
+     *  Pressure
+     *  Temperature
+     *
+     * @author          Shakhawat Hossain
+     * @version         1.0
+     * @since           11/23/2019
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +71,8 @@ public class MainActivity extends AppCompatActivity
         mTextSensorLight = (TextView) findViewById(R.id.label_light);
         mTextSensorProximity = (TextView) findViewById(R.id.label_proximity);
         mTextSensorTemp = (TextView) findViewById(R.id.label_temp);
+        mTextSensorHumid = (TextView) findViewById(R.id.label_humidity);
+        mTextSensorPressure = (TextView) findViewById(R.id.label_pressure);
 
         // Get an instance of the sensor manager.
         mSensorManager = (SensorManager) getSystemService(
@@ -51,6 +84,8 @@ public class MainActivity extends AppCompatActivity
         mSensorLight = mSensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
         mSensorProximity = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
         mSensorTemp = mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+        mSensorHumid = mSensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY);
+        mSensorPressure = mSensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
 
         // Get the error message from string resources.
         String sensor_error = getResources().getString(R.string.error_no_sensor);
@@ -60,6 +95,8 @@ public class MainActivity extends AppCompatActivity
         if (mSensorLight == null) { mTextSensorLight.setText(sensor_error); }
         if (mSensorProximity == null) { mTextSensorProximity.setText(sensor_error); }
         if (mSensorTemp == null) { mTextSensorTemp.setText(sensor_error); }
+        if (mSensorHumid == null) { mTextSensorHumid.setText(sensor_error); }
+        if (mSensorPressure == null) { mTextSensorPressure.setText(sensor_error); }
     }
 
     @Override
@@ -85,6 +122,16 @@ public class MainActivity extends AppCompatActivity
 
         if (mTextSensorTemp != null) {
             mSensorManager.registerListener(this, mSensorTemp,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+
+        if (mTextSensorHumid != null){
+            mSensorManager.registerListener(this, mSensorHumid,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+
+        if (mTextSensorPressure != null){
+            mSensorManager.registerListener(this, mSensorPressure,
                     SensorManager.SENSOR_DELAY_NORMAL);
         }
     }
@@ -116,7 +163,8 @@ public class MainActivity extends AppCompatActivity
                 // from the resources, with the placeholder filled in.
                 mTextSensorLight.setText(getResources().getString(
                         R.string.label_light, currentValue));
-                Log.d("Light", Double.toString(currentValue));
+                setAmbient_light(currentValue);
+                Log.d("Light", Double.toString(getAmbient_light()));
                 break;
 
             case Sensor.TYPE_PROXIMITY:
@@ -128,8 +176,22 @@ public class MainActivity extends AppCompatActivity
             case Sensor.TYPE_AMBIENT_TEMPERATURE:
                 mTextSensorTemp.setText(getResources().getString(
                         R.string.label_temp, currentValue));
-                Log.d("Temperature", Double.toString(currentValue));
+                setTemperature(currentValue);
+                Log.d("Temperature", Double.toString(getTemperature()));
+                break;
 
+            case Sensor.TYPE_RELATIVE_HUMIDITY:
+                mTextSensorHumid.setText(getResources().getString(
+                        R.string.label_humidity, currentValue));
+                setHumidity(currentValue);
+                Log.d("Humidity", Double.toString(getHumidity()));
+                break;
+
+            case Sensor.TYPE_PRESSURE:
+                mTextSensorPressure.setText(getResources().getString(
+                        R.string.label_pressure, currentValue));
+                setPressure(currentValue);
+                Log.d("Pressure", Double.toString(getPressure()));
 
             default:
                 // do nothing
@@ -144,4 +206,86 @@ public class MainActivity extends AppCompatActivity
     public void onAccuracyChanged(Sensor sensor, int i) {
     }
 
+    /**
+     * POJO- setters
+     * Getting sensor data and display it
+     *
+     * Sensor used:
+     *  Ambient Light
+     *  Humidity
+     *  Pressure
+     *  Temperature
+     *
+     * @param humidity
+     *
+     * @author          Shakhawat Hossain
+     * @version         1.0
+     * @since           11/23/2019
+     */
+    public void setHumidity(float humidity) {
+        this.humidity = humidity;
+    }
+
+    /**
+     *@param ambient_light
+     *
+     * @author          Shakhawat Hossain
+     * @version         1.0
+     * @since           11/23/2019
+     */
+    public void setAmbient_light(float ambient_light) {
+        this.ambient_light = ambient_light;
+    }
+
+    /**
+     *@param pressure
+     *
+     * @author          Shakhawat Hossain
+     * @version         1.0
+     * @since           11/23/2019
+     */
+    public void setPressure(float pressure) {
+        this.pressure = pressure;
+    }
+
+    /**
+     *@param temperature
+     *
+     * @author          Shakhawat Hossain
+     * @version         1.0
+     * @since           11/23/2019
+     */
+    public void setTemperature(float temperature) {
+        this.temperature = temperature;
+    }
+
+    /**
+     * POJO- getters
+     * Getting sensor data and display it
+     *
+     * Sensor used:
+     *  Ambient Light
+     *  Humidity
+     *  Pressure
+     *  Temperature
+     *
+     * @author          Shakhawat Hossain
+     * @version         1.0
+     * @since           11/23/2019
+     */
+    public float getAmbient_light() {
+        return ambient_light;
+    }
+
+    public float getHumidity() {
+        return humidity;
+    }
+
+    public float getPressure() {
+        return pressure;
+    }
+
+    public float getTemperature() {
+        return temperature;
+    }
 }
